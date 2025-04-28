3dc5e284de2159153aa50a2f3ce7064cad68c5b3
alexanderr4 <jose231101a@gmail.com>
2025-04-28 15:21:22 -0500

    current error

--- src/components/home/componentsHome/summary/Summary.js
+++ src/components/home/componentsHome/summary/Summary.js
@@ -112,7 +112,7 @@ function Summary() {
 
     const sortedRequests = () => {
         try {
-            if (dataCancellations.current) {
+            if (dataCancellations.current && dataCancellations.current.data.length > 0) {
 
                 const sortedRequests = [...dataCancellations.current.data].sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
     
