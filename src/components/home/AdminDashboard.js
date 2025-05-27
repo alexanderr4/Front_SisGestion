00f42d174240f21f7ae538d246497ee0d1d15619
alexanderr4 <jose231101a@gmail.com>
2025-05-27 11:13:50 -0500

    fix(AdminDashboard): remove authToken state from useEffect dependencies for improved performance

--- src/components/home/AdminDashboard.js
+++ src/components/home/AdminDashboard.js
@@ -25,7 +25,6 @@ function AdminDashboard() {
   const [isMenuVisible, setMenuVisible] = useState(true);
   const [isOverlayVisible, setOverlayVisible] = useState(false);
   const pathInitial = window.location.pathname;
-  const [authToken, setAuthToken] = useState(localStorage.getItem('authToken'));
 
   useEffect(() => {
     try {
@@ -50,7 +49,7 @@ function AdminDashboard() {
       window.location.href = '/login';
     }
 
-  }, [pathInitial, navigate, authToken, location.pathname]);
+  }, [navigate, location.pathname]);
 
   useEffect(() => {
     setLoading(false);
